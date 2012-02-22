c97d59d46a1157a2c4ebe908b4adfe73f7374895
dimitri fedorov <df@antistatic.net>
2012-02-22 14:59:41 +0400

    Fixing setSelectorPaintCoeff for visibleItems == 2

--- library/src/antistatic/widget/wheel/WheelVerticalView.java
+++ library/src/antistatic/widget/wheel/WheelVerticalView.java
@@ -111,34 +111,42 @@ public class WheelVerticalView extends AbstractWheelView {
 
     @Override
     public void setSelectorPaintCoeff(float coeff) {
+        LinearGradient shader;
 
         int h = getMeasuredHeight();
         int ih = getItemDimension();
-
         float p1 = (1 - ih/(float) h)/2;
         float p2 = (1 + ih/(float) h)/2;
-        float p3 = (1 - ih*3/(float) h)/2;
-        float p4 = (1 + ih*3/(float) h)/2;
-
-        float s = 255 * p3/p1;
         float z = mItemsDimmedAlpha * (1 - coeff);
-
-        float c3f = s * coeff ; // here goes some optimized stuff
-        float c2f = z + c3f;
         float c1f = z + 255 * coeff;
 
-        int c1 = Math.round( c1f ) << 24;
-        int c2 = Math.round( c2f ) << 24;
-        int c3 = Math.round( c3f ) << 24;
+        if (mVisibleItems == 2) {
+            int c1 = Math.round( c1f ) << 24;
+            int c2 = Math.round( z ) << 24;
+            int[] colors =      {c2, c1, 0xff000000, 0xff000000, c1, c2};
+            float[] positions = { 0, p1,     p1,         p2,     p2,  1};
+            shader = new LinearGradient(0, 0, 0, h, colors, positions, Shader.TileMode.CLAMP);
+        } else {
+            float p3 = (1 - ih*3/(float) h)/2;
+            float p4 = (1 + ih*3/(float) h)/2;
 
-        int[] colors =      {0, c3, c2, c1, 0xff000000, 0xff000000, c1, c2, c3, 0};
-        float[] positions = {0, p3, p3, p1,     p1,         p2,     p2, p4, p4, 1};
+            float s = 255 * p3/p1;
+            float c3f = s * coeff ; // here goes some optimized stuff
+            float c2f = z + c3f;
 
-        LinearGradient shader = new LinearGradient(0, 0, 0, h, colors, positions, Shader.TileMode.CLAMP);
+            int c1 = Math.round( c1f ) << 24;
+            int c2 = Math.round( c2f ) << 24;
+            int c3 = Math.round( c3f ) << 24;
+
+            int[] colors =      {0, c3, c2, c1, 0xff000000, 0xff000000, c1, c2, c3, 0};
+            float[] positions = {0, p3, p3, p1,     p1,         p2,     p2, p4, p4, 1};
+            shader = new LinearGradient(0, 0, 0, h, colors, positions, Shader.TileMode.CLAMP);
+        }
         mSelectorWheelPaint.setShader(shader);
-
         invalidate();
     }
+
+
     //--------------------------------------------------------------------------
     //
     //  Scroller-specific methods
