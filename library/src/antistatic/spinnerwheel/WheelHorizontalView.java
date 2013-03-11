e9a3dbc020286a2130652175764d30f10b8b8d37
Aleksey Malevaniy <a.malevaniy@gmail.com>
2013-03-11 20:56:41 +0200

    Items dimmed Alpha '100' removes dimming at all

--- library/src/antistatic/spinnerwheel/WheelHorizontalView.java
+++ library/src/antistatic/spinnerwheel/WheelHorizontalView.java
@@ -111,6 +111,9 @@ public class WheelHorizontalView extends AbstractWheelView {
 
     @Override
     public void setSelectorPaintCoeff(float coeff) {
+        if (mItemsDimmedAlpha >= 100)
+            return;
+
         LinearGradient shader;
 
         int w = getMeasuredWidth();
@@ -138,8 +141,8 @@ public class WheelHorizontalView extends AbstractWheelView {
             int c2 = Math.round( c2f ) << 24;
             int c3 = Math.round( c3f ) << 24;
 
-            int[] colors =      {0, c3, c2, c1, 0xff000000, 0xff000000, c1, c2, c3, 0};
-            float[] positions = {0, p3, p3, p1,     p1,         p2,     p2, p4, p4, 1};
+            int[] colors = { c2, c2, c2, c2, 0xff000000, 0xff000000, c2, c2, c2, c2 };
+            float[] positions = { 0, p3, p3, p1, p1, p2, p2, p4, p4, 1 };
             shader = new LinearGradient(0, 0, w, 0, colors, positions, Shader.TileMode.CLAMP);
         }
         mSelectorWheelPaint.setShader(shader);
